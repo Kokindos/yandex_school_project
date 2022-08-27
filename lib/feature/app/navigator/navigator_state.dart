1c6327d2c0b9505498f2efa892edae848ad30e88
Kokindos <kokindoss@gmail.com>
2022-08-27 16:08:03 +0300

    fix

--- lib/feature/app/navigator/navigator_state.dart
+++ lib/feature/app/navigator/navigator_state.dart
@@ -4,11 +4,13 @@ abstract class NavigatorConfigState {}
 
 class NavigatorListState extends NavigatorConfigState {}
 
+
 class NavigatorTaskState extends NavigatorConfigState {
   final String id;
 
   NavigatorTaskState({required this.id});
 }
+
 
 class NavigatorNewTaskState extends NavigatorConfigState {
 }
